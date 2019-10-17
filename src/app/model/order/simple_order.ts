91f7b37945aa7560e2837dac46bd1ee7a36c13d9
Liu Lei <greenborn101@gmail.com>
2019-10-17 14:46:37 -0400

    added tip field

--- src/app/model/order/simple_order.ts
+++ src/app/model/order/simple_order.ts
@@ -19,6 +19,7 @@ export class SimpleOrder {
         "id",
         "status",
         "total",
+        "tip",
         "penalty",
         "kegsDeliveredQty",
         "tapsDeliveredQty",
@@ -29,6 +30,7 @@ export class SimpleOrder {
     status: number;
     total: number;
     penalty: number;
+    tip: number;
 
     deliveredAt: Date;
     returnedAt: Date;
